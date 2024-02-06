dcbd8c97571a887f265bd98e48a7fde205799ced
Jaideep Soni <jaideepsoni29@gmail.com>
2024-02-07 02:30:23 +0530

    add CSS to structure

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './LoginForm.css'
+import './LoginForm.css';
+import { FaUser } from "react-icons/fa6";
+import { FaLock } from "react-icons/fa";
 
 export const LoginForm = () => {
     return (
@@ -7,15 +9,17 @@ export const LoginForm = () => {
             <h1>Login</h1>
             <div className='input-box'>
                 <input type="text" placeholder='Username' required />
+                <FaUser className='icon'/>
             </div>
             <div className='input-box'>
                 <input type="password" placeholder='password' required />
+                <FaLock className='icon'/>
             </div>
             <div className='remember-forgot'>
                 <label><input type="checkbox" />Remeber me</label>
                 <a href="/">Forgot password?</a>
             </div>
-            <button type='submit'>Login</button>
+            <button className = 'btn' type='submit'>Login</button>
             <div className='register-link'>
                 <p>Dont have an account? <a href="/">Register here</a> </p>
             </div>
